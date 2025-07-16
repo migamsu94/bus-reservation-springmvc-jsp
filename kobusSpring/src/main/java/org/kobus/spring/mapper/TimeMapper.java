ad9cd277896cd747b30d2038926e7fec1382b686
athena1998 <athena1998@naver.com>
2025-07-16 16:35:32 +0900

    koBus Spring legacy project 변환

---
+++ kobusSpring/src/main/java/org/kobus/spring/mapper/TimeMapper.java
@@ -0,0 +1,12 @@
+package org.kobus.spring.mapper;
+
+import org.apache.ibatis.annotations.Select;
+
+public interface TimeMapper {	
+	
+	String getTime();
+	
+	@Select("SELECT sysdate + 1 FROM dual")
+	String getNextTime();
+
+}
